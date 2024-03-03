1430a458c1dc8d448adfe545ddb18fae81c38978
hashilyze <hashilyze@gmail.com>
2024-03-03 16:11:18 +0900

    [This Is Java] Ch1

---
+++ src/main/java/this_is_java/ch1/ex1/Calculator.java
@@ -0,0 +1,10 @@
+package this_is_java.ch1.ex1;
+
+public class Calculator {
+    public static void main(String args[]){
+        int x = 1;
+        int y = 2;
+        int result = x + y;
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/this_is_java/ch1/ex2/Verify.java
@@ -0,0 +1,7 @@
+package this_is_java.ch1.ex2;
+
+public class Verify {
+    public static void main(String args[]){
+        System.out.println("개발자가 되기 위한 필수 개발 언어 Java");
+    }
+}
